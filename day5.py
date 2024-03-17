c97f835eeae128d869a865b5c2cf88654508d131
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2024-03-18 05:11:49 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,18 @@
+import random
+
+char=['q','w','e','r','t','y','u','i','o','p','l','k','j','h','g','f','d','s','a','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','L','K','J','H','G','F','D','S','A','Z','X','C','V','B','N','M']
+nums=['1','2','3','4','5','6','7','8','9','0']
+special_chars=[ '!','@','#','$','%','^','&','&','*','_','+','-','=',':',';','<',''>',','.','?','/','|']
+n=int(input("How many letters do you want?"))
+m=int(input("How many digits do you want?"))
+s=int(input("How many special characters do you want?"))
+password=''
+for i in range (n+1):
+    password+=random.choice(char)
+for j in range (m+1):
+    password+=random.choice(nums)
+for k in range (s+1):
+    password+=random.choice(special_chars)
+print(password)
+    
+
